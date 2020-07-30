c784bad077b68d224a371d6b6d91badf6ab17040
Vincent Hwang <Vincenth421@users.noreply.github.com>
2020-07-29 20:35:40 -0700

    Update Chapter1.java

--- Chapter1.java
+++ Chapter1.java
@@ -25,7 +25,8 @@ public class Chapter1{
      //No data structure
      public static boolean isUniqueNoDS(String str)
      {
-          if (str.length() > 26) { // Only 26 characters
+	  // Only 26 characters
+          if (str.length() > 26) { 
 			return false;
 		}
 
@@ -33,10 +34,10 @@ public class Chapter1{
 		for (int i = 0; i < str.length(); i++) {
 			int val = str.charAt(i) - 'a';
 
-               //shift 1 left according to ascii of current char - 'a'. If there is already a 1 there, character is not unique.
+               		//shift 1 left according to ascii of current char - 'a'. If there is already a 1 there, character is not unique.
 			if ((checker & (1 << val)) > 0) return false;
 
-               //otherwise add a 1 to the checker at current char - 'a'
+               		//otherwise add a 1 to the checker at current char - 'a'
 			checker |= (1 << val);
 		}
 		return true;
